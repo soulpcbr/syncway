c52a8e225bf8a7696a53adf5d94a91080be797c7
victo <victo@VLA-NOTE>
2017-05-23 12:50:15 -0300

    Adicionado pasta de teste com o server01 de testes. O server 01 tem uma página em php que faz o tratamento do upload de arquivos, semelhante ao que será usado no servidor real.

---
+++ teste/server01/receiver.php
@@ -0,0 +1,57 @@
+<?php
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+    $data = array();
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $data['status'] = 0;
+    } else {
+        $data['status'] = 1;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $data['status'] = 0;
+    } else {
+        $data['status'] = 1;
+    }
+    // Allow certain file formats
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $data['status'] = 0;
+    } else {
+        $data['status'] = 1;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($data['status'] == 0) {
+        $data['msg'] = "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            $data['msg'] = "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+        } else {
+            $data['msg'] = "Sorry, there was an error uploading your file.";
+        }
+    }
+    echo json_encode($data);
+    exit();
+} else {
+    ?>
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <form action="./receiver.php" method="post" enctype="multipart/form-data">
+                Select image to upload:
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="text" name="param" id="param" value="" />
+                <input type="submit" value="Upload Image" name="submit">
+            </form>
+        </body>
+    </html>
+<?php } ?>
