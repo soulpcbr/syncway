0b5d2461821f9facd24c0fe03148f0ed466a1120
Italo Castilho <icastilho@craftcode.com.br>
2017-06-08 15:57:13 -0300

    Alteração para habilitar cadastro do admin na primeira execução

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AuthService } from './services/auth.service';
+import {UserService} from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  isFirstExecution = false;
+
+  constructor(public auth: AuthService,
+              private userService: UserService) { }
+
+  ngOnInit() { console.log('ngOnInit:');
+    this.userService.countUsers().subscribe(
+      (value) => { console.log('value:', value);
+        this.isFirstExecution = (value === 0); }
+    );
+  }
 
 }
