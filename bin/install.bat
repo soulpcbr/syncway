9faaf8b8b604872079fdcc8f9de255497d05780c
Italo Castilho <icastilho@craftcode.com.br>
2017-12-21 17:35:37 -0200

    add ffmpeg variables to install script

--- bin/install.bat
+++ bin/install.bat
@@ -5,12 +5,14 @@ set "CURRENT_DIR=%cd%"
 SET PATH=%~dp0
 SET SERVICE_EXE=%"%\node.exe server\app.js%"%
 SET SERVICENAME=%"%Syncway%"%
+SET FFMPEG_PATH="C:\work\Syncway\ffmpeg\bin\ffmpeg.exe"
+SET FFPROBE_PATH=C:\work\Syncway\ffmpeg\bin\ffprobe.exe"
 
 :install
 ECHO Installing service "%SERVICENAME%"
 cd "%PATH%"
 nssm install %SERVICENAME% %PATH%%SERVICE_EXE%
-nssm set %SERVICENAME% AppEnvironmentExtra SECRET_TOKEN=syncthewayitgone
+nssm set %SERVICENAME% AppEnvironmentExtra SECRET_TOKEN=syncthewayitgone FFMPEG_PATH="%CURRENT_DIR%\ffmpeg\bin\ffmpeg.exe" FFPROBE_PATH="%CURRENT_DIR%\ffmpeg\bin\ffprobe.exe"
 cd "%CURRENT_DIR%"
 GOTO end
 :fail
